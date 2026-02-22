5245eca838409e918048d384f7fc64045aa3e6c0
TheBobGottfriedFan <turnerwarner78@gmail.com>
2026-02-22 12:12:33 -0500

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login"),
+    path("register/", views.register_view, name="register")
     path("dashboard/", views.dashboard_view, name="dashboard"),
     path("logout/", views.logout_view, name="logout"),
 ]
