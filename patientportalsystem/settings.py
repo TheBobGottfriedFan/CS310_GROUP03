c557a238850435931d630443535182d7ba74e3a5
TheBobGottfriedFan <turnerwarner78@gmail.com>
2026-02-22 12:25:01 -0500

    Update settings.py

--- patientportalsystem/settings.py
+++ patientportalsystem/settings.py
@@ -1,1 +1,96 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+#BASE DIRECTORY
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# ENVIRONMENT VARIABLES
+load_dotenv(BASE_DIR / ".env")
+
+#SECURITY
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ValueError("DJANGO_SECRET_KEY not set in the root .env file")
+
+DEBUG = True # For Debugging, in Black-Box Testing this MUST be set to FALSE.
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+# Definitions
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "portal",  # Application
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "patientportalsystem.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],  # App templates auto-discovered
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "patientportalsystem.wsgi.application"
+
+#MySQL Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
+    }
+}
+
+# Languages
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "America/New_York"
+USE_I18N = True
+USE_TZ = True
+
+# Static Files
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# PRIMARILY FIELD KEY
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Security Debugging Tools
+if not DEBUG:
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    X_FRAME_OPTIONS = "DENY"
