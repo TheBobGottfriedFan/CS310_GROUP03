1c89b157f87aa929d32b63223e9adbab303e7044
TheBobGottfriedFan <turnerwarner78@gmail.com>
2026-02-22 12:07:28 -0500

    Update urls.py

--- patientportalsystem/urls.py
+++ patientportalsystem/urls.py
@@ -1,1 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
 
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("portal.urls")),  # send site root to portal app
+]
