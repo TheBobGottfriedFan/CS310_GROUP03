eb33cc843a2058eb2e063ce90c4096082f7f24c6
TheBobGottfriedFan <turnerwarner78@gmail.com>
2026-02-22 12:09:23 -0500

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -1,1 +1,9 @@
+# portal/urls.py
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path("", views.login_view, name="login"),
+    path("dashboard/", views.dashboard_view, name="dashboard"),
+    path("logout/", views.logout_view, name="logout"),
+]
