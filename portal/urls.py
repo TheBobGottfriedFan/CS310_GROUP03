ef7e3a761d837aec79f42a621801dd50083cafcd
TheBobGottfriedFan <turnerwarner78@gmail.com>
2026-02-22 12:14:04 -0500

    Update urls.py
    forgot a comma

--- portal/urls.py
+++ portal/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login"),
-    path("register/", views.register_view, name="register")
+    path("register/", views.register_view, name="register"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
     path("logout/", views.logout_view, name="logout"),
 ]
