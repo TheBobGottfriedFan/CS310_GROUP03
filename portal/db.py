e384cf9451dbe8397efbcea5cf75e4dfedaa165d
kendraadkins <kendra.adkins01@gmail.com>
2026-04-08 14:38:39 -0400

    local setup fixes (db + settings + views)

--- portal/db.py
+++ portal/db.py
@@ -1,14 +1,13 @@
 # portal/db.py
+
 import mysql.connector
-from django.conf import settings
 
 def get_connection():
-    db = settings.DATABASES["default"]
     return mysql.connector.connect(
-        host=db.get("HOST", "localhost"),
-        user=db.get("USER", ""),
-        password=db.get("PASSWORD", ""),
-        database=db.get("NAME", ""),
-        port=int(db.get("PORT", 3306)),
+        host="localhost",
+        user="root",
+        password="Starbucks2022!",
+        database="patient_portal",
+        port=3306,
         autocommit=True,
     )
--- portal/views.py
+++ portal/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout, messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .db import get_connection
 from .rbac import require_login, require_permission, require_any_permission
 from .permissions import (
