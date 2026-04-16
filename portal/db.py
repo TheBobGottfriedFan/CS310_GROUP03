2a46e9d7664d3b68dcd84d03ecc8cf5f93c334ad
jeffreyjezewski21-ship-it <jeffreyjezewski21@gmail.com>
2026-04-16 15:06:24 -0500

    Update database connection to use dotenv
    Refactor database connection to use environment variables.

--- portal/db.py
+++ portal/db.py
@@ -1,13 +1,16 @@
 # portal/db.py
-
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Mookie123*",
-        database="patient_portal",
-        port=3306,
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT")),
         autocommit=True,
     )
